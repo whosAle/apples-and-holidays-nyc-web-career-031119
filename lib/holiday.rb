57ccd9b771066dcb8875ea5d5fea7e46e771cb3b
A Belgrave <abelgrave95@gmail.com>
2019-02-27 17:24:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday.each do |day, supply|
       day = day.to_s.split
-      day.each {|word| word.capitalize}
+      day.each {|word| word.capitalize!}
       day.join(" ")
       # puts "#{day.to_s.split.each {|word| word.capitalize}.join(" ")}: #{supply.join(" ")}" }
       puts "#{day}: #{supply.join(" ")}"
